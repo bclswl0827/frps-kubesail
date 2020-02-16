835c425854ad5cd57f7e0380c05e05675e149591
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-02-16 21:03:06 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Add Frps configuration
-echo "\ntoken = $FRP_TOKEN\nvhost_http_port = 80\ntcp_mux = $TCP_MUX" >> /usr/bin/frps/frps.ini
+echo "\nvhost_http_port = 80" >> /usr/bin/frps/frps.ini
 # Run Frps
 /usr/bin/frps/frps -c /usr/bin/frps/frps.ini
